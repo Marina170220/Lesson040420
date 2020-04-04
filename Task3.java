bfab6f0f91abc1cf97a28f00af7b722da4172aa5
Marina <petrukmn@yandex.ru>
2020-04-04 13:19:23 +0300

    task #3

---
+++ Task3.java
@@ -0,0 +1,13 @@
+package KompjuterIya.Lesson040420;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String str = "The weather is fine";
+        if (str.length() > 5) {
+            System.out.println(str.substring(0, 3) + ", " + str.substring(str.length() - 3, str.length()));
+        } else for (char c = 0; c < str.length(); c++) {
+            System.out.print(str.charAt(0));
+        }
+
+    }
+}
