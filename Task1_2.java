676e67397a34d2f844f31b5b3012a29fafa2859b
Marina <petrukmn@yandex.ru>
2020-04-04 13:19:04 +0300

    task #1,2

---
+++ Task1_2.java
@@ -0,0 +1,16 @@
+package KompjuterIya.Lesson040420;
+
+public class Task1_2 {
+    public static void main(String[] args) {
+
+        String str = "I'm learning Java";
+        System.out.println("Task 1");
+        System.out.println(str + ", " + str + ", " + str);
+        System.out.println("Number of elements in string: " + str.length());
+        System.out.println();
+        System.out.println("Task 2");
+        System.out.println("First element: " + str.charAt(0));
+        System.out.println("Last element: " + str.charAt(str.length() - 1));
+        System.out.println("Middle element: " + str.charAt((str.length() - 1) / 2));
+    }
+}
